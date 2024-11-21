3f38d7888c34978e4b7596a2ed60e9f00844e1e6
lucas bittencourt rauch <lucas.bittencourtrauch@gmail.com>
2024-11-21 17:04:13 -0300

    criado formulario de inclusao de noticia

---
+++ src/app/gerenciar/novaNoticia/page.tsx
@@ -0,0 +1,44 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { CloudUpload } from "lucide-react";
+
+export default function novaNoticia() {
+  return (
+    <div className="flex justify-center">
+      <Card className="max-w-[1000px] flex-1">
+        <CardHeader>
+          <CardTitle>Nova Noticia</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-5">
+            <div>
+              <Label>Titulo</Label>
+              <Textarea placeholder="Nova especie de moluscos descoberta" />
+            </div>
+            <div>
+              <Label>Descricao (opcional)</Label>
+              <Textarea placeholder="Nova especie de moluscos descoberta" />
+            </div>
+            <div>
+              <Label>Capa da noticia</Label>
+              <div>
+                <Input type="file" id="capaInput" className="hidden" />
+                <Label
+                  htmlFor="capaInput"
+                  className="w-full flex justify-center h-20 border rounded-md items-center"
+                >
+                  <div className="flex-col items-center flex">
+                    <CloudUpload />
+                    <Label>Escolha o arquivo</Label>
+                  </div>
+                </Label>
+              </div>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
